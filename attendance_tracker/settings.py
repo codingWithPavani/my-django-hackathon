6cb86a45d841459e4d3a3dd0911f27642c681bff
codingwithpavani <abothulapavni162gmail.com>
2025-08-16 16:54:07 +0530

    updates database

--- attendance_tracker/settings.py
+++ attendance_tracker/settings.py
@@ -85,11 +85,15 @@ DATABASES = {
 }
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default='postgresql://my_hackathon_db_user:4FYAOWthKgVESxaTC5d4OM6giHHLAFZF@dpg-d2diubgdl3ps73b34vbg-a/my_hackathon_db'
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'my_hackathon_db',
+        'USER': 'my_hackathon_db_user',
+        'PASSWORD': '4FYAOWthKgVESxaTC5d4OM6giHHLAFZF',
+        'HOST': 'dpg-d2diubgdl3ps73b34vbg-a.oregon-postgres.render.com',
+        'PORT': '5432',
+    }
 }
-
 
 
 
